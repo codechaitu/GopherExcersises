aa6e4835772f165ba34c3fea69a29d1fdd5c7b8d
codechaitu <codechaitu@mercari.com>
2019-03-15 13:37:05 +0900

    Adding timer to each question

--- QuizGame_1/quiz.go
+++ QuizGame_1/quiz.go
@@ -9,13 +9,17 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
+
+var MAX_TIME_FOR_QUESTION = 5 * time.Second // in seconds
 
 func main(){
 	var (
 		numOfQuestions int
 		numOfCorrectAnswers int
 		userAnswer string
+
 	)
 	// Read CSV file
 	filenamePointer := flag.String("file", "problems", "input file name for quiz")
@@ -31,12 +35,28 @@ func main(){
 		}else if(err != nil){
 			log.Fatal("Error: ", err)
 		}
+
+		// Print the question
 		numOfQuestions = numOfQuestions + 1
 		fmt.Println("Question: ", line[0])
-		fmt.Scanln(&userAnswer)
-		if(userAnswer == strings.TrimSpace(line[1])){
-			numOfCorrectAnswers = numOfCorrectAnswers + 1
+
+		// Wait for answer in another go routine
+		ch := make(chan int)
+		go func() {
+			fmt.Scanln(&userAnswer)
+			ch <- 1
+		}()
+
+		select {
+		case <-ch:
+			if(userAnswer == strings.TrimSpace(line[1])){
+				numOfCorrectAnswers = numOfCorrectAnswers + 1
+			}
+			break
+		case <-time.After(MAX_TIME_FOR_QUESTION):
+			fmt.Println("Time out")
 		}
+
 
 	}
 
